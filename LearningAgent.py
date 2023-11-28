799723a47fc99ed8373ca0b42d968314363cd8bf
ALoNePoMaHTiK <79035908+ALoNePoMaHTiK@users.noreply.github.com>
2023-11-28 14:56:14 +0300

    Add files via upload

--- LearningAgent.py
+++ LearningAgent.py
@@ -1,6 +1,9 @@
 import csv
 from itertools import product
 
+WIN = 'Win'
+FAIL = 'Fail'
+DEAD = 'Dead'
 
 class LearningAgent():
 
@@ -9,31 +12,34 @@ class LearningAgent():
         self.steps = []
         self.map = ''
         self.player = 'O'
-        self.Winning = 0
+        self.Winning = 0 
 
-    #Определение завершения игры
-    #Если winnerCheck==True, то определяет победил ли агент
-    def isEnd(self,map,winnerCheck=False):
-        rows = [map[:3], map[3:-3], map[-3:],
-                map[::3], map[1::3], map[2::3],
-                map[0::4], map[6:0:-2]]
-        if winnerCheck:
-            return any([row in [self.player*3] for row in rows])
-        return any([row in ['XXX','OOO'] for row in rows])
+    #Получение результата игры
+    def getGameResult(self):
+        rows = [self.map[:3], self.map[3:-3], self.map[-3:],
+                self.map[::3], self.map[1::3], self.map[2::3],
+                self.map[0::4], self.map[6:0:-2]]
+        if any([row in [self.player*3] for row in rows]):
+            return WIN
+        elif any([row in ['XXX'] for row in rows]):     #Подумать над строгостью Х
+            return FAIL
+        else:
+            return DEAD
 
+    # Генерация стандартной матрицы состояний
     def generateClearStateMatrix(self):
         states = dict()
         for i in product(' XO', repeat=9):
             word = ''.join(i)
             value = 0.5
-            if self.isEnd(word):
-                value = 1.0
             states[word] = (len(states), value)
         return states
 
+    # Сеттер для карты
     def setMap(self,map):
         self.map = map
 
+    # Получение индексов пустых полей
     def getFree(self):
         free = []
         for i in range(len(self.map)):
@@ -41,9 +47,10 @@ class LearningAgent():
                 free.append(i)
         return free
 
+    # Ход на основании матрицы состояний
     def step(self):
         free = self.getFree()
-        value = 0
+        value = -1
         preferedState = ''
         for index in free:
             state = self.map[:index] + self.player + self.map[index + 1:]
@@ -51,29 +58,33 @@ class LearningAgent():
             if stateValue[1] > value:
                 value = stateValue[1]
                 preferedState = state
-        self.steps.append((preferedState))
+        self.steps.append(preferedState)
         self.map = preferedState
 
-    def saveAgent(self):
-        with open('Agent.csv', 'w', newline='') as csvfile:
+    # Сохранение агента в файл
+    def saveAgent(self,path):
+        with open(path, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=';')
             for state in self.leaningMatrix.keys():
                 value = self.leaningMatrix[state]
                 writer.writerow((state,value[0],value[1]))
 
+    # Загрузка агента из файла
     def loadAgent(self,path):
         with open(path, 'r', newline='') as csvfile:
             reader = csv.reader(csvfile,delimiter=';')
             for state in reader:
                 self.leaningMatrix[state[0]] = (int(state[1]),float(state[2]))
+
+    # Сброс шагов игры
     def restart(self):
         self.steps = []
 
+    # Обучение на основании шагов и результата
     def learn(self):
-        isWinner = self.isEnd(self.map,winnerCheck=True)
-        self.Winning += int(isWinner)
-        valueDiff = 0.1 if isWinner else 0
+        result = self.getGameResult()
+        if result == WIN:
+            self.Winning += 1
         for step in self.steps:
-            #print(step,end='->')
             temp = self.leaningMatrix[step]
-            self.leaningMatrix[step] = (temp[0],temp[1]+valueDiff)
+            self.leaningMatrix[step] = (temp[0],temp[1]+0.4*((result==WIN)-temp[1]))
--- TicTacToe.py
+++ TicTacToe.py
@@ -1,43 +1,24 @@
-from itertools import product
+import os
 from random import randint
-import csv
 import matplotlib.pyplot as plt
 
 import LearningAgent
 
 def out(row):
-    print(f'\t┏━━━┓')
-    print(f'\t┃{row[:3]}┃')
-    print(f'\t┃{row[3:-3]}┃')
-    print(f'\t┃{row[-3:]}┃')
-    print(f'\t┗━━━┛')
+    print(f' ┏━━━┓')
+    print(f' ┃{row[:3]}┃')
+    print(f' ┃{row[3:-3]}┃')
+    print(f' ┃{row[-3:]}┃')
+    print(f' ┗━━━┛')
 
+# Определение завершенности игры
 def isEnd(map):
     rows = [map[:3], map[3:-3], map[-3:],
             map[::3], map[1::3], map[2::3],
             map[0::4], map[6:0:-2]]
     return any([row in ['XXX', 'OOO'] for row in rows]) or map.count(' ') == 0
-def generateClearStateMatrixInFile():
-    states = []
-    for i in product(' XO',repeat=9):
-        word = ''.join(i)
-        value = 0.5
-        if isEnd(word):
-            value = 1.0
-        states.append((len(states),word,value))
-    with open('States.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile,delimiter=';')
-        writer.writerows(states)
-def generateClearStateMatrix():
-    states = dict()
-    for i in product(' XO', repeat=9):
-        word = ''.join(i)
-        value = 0.5
-        if isEnd(word):
-            value = 1.0
-        states[word] =(len(states),value)
-    return states
 
+# Получение индексов пустых полей
 def getFree(map):
     free = []
     for i in range(len(map)):
@@ -45,39 +26,65 @@ def getFree(map):
             free.append(i)
     return free
 
+# Случайный ход крестиком
 def staticAgent(map:str):
     free = getFree(map)
     player = 'O' if map.count('X')>map.count('O') else 'X'
     index = free[randint(0,len(free)-1)]
     return map[:index]+player + map[index+1:]
 
-
-la = LearningAgent.LearningAgent()
-la.loadAgent('Agent.csv')
-winners = []
-WinningSum = 0
-for i in range(10000):
-    map = ' '*9
+# Игра с агентом
+def playWithAgent():
+    la = LearningAgent.LearningAgent()
+    la.loadAgent('Agent.csv')
+    map = ' ' * 9
+    free = getFree(map)
+    step = -1
     while not isEnd(map):
-        map = staticAgent(map)
-        la.setMap(map)
+        while step not in free:
+            out(map)
+            step = int(input())
+            os.system('cls')
+        map = map[:step] + 'X' + map[step+1:]
+        step = -1
+        out(map)
         if isEnd(map):
             break
+        la.setMap(map)
         la.step()
         map = la.map
-    la.learn()
-    la.restart()
-    if i%50==0 and i!=0:
-        winners.append(la.Winning-WinningSum)
-        WinningSum+= la.Winning
-    print(f'#{i}')
+        os.system('cls')
 
-la.saveAgent()
-plt.plot(winners)
-plt.show()
+# Тренировка агента
+def TrainAgent():
+    la = LearningAgent.LearningAgent()
+    if os.path.exists('Agent.csv'):
+        la.loadAgent('Agent.csv')
+    winners = []
+    WinningLast = 0
+    for i in range(10000000):
+        map = ' '*9
+        while not isEnd(map):
+            map = staticAgent(map)
+            if isEnd(map):
+                break
+            la.setMap(map)
+            la.step()
+            map = la.map
+        la.learn()
+        la.restart()
+        if i%50000==0 and i!=0:
+            winners.append((la.Winning-WinningLast)/50000*100)
+            WinningLast= la.Winning
+        print(f'#{i}')
+    print(winners)
+    la.saveAgent('Agent.csv')
+    plt.plot(winners)
+    plt.savefig('Learn.png')
+    plt.show()
 
-
-
+#TrainAgent()
+playWithAgent()
 
 
 
